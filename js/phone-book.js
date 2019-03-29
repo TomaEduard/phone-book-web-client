05dddb0345d36732b0498839ace5de96256be6d7
Eduard Toma <eduard.daniel.toma@gmail.com>
2019-03-29 17:18:51 +0200

    Delete function

--- js/phone-book.js
+++ js/phone-book.js
@@ -24,6 +24,7 @@ window.PhoneBook = {
         }).done(function (response) {
             console.log(response);
             // reload items table
+            PhoneBook.getItems();
         });
     },
 
@@ -35,12 +36,15 @@ window.PhoneBook = {
     <td class="lastname">${item.lastname}</td>
     <td class="number">${item.number}</td>
     <td class="email">${item.email}</td>
+    <td><a href="#" class="fa fa-trash fa-lg text-danger delete" data-id="${item.id}"></a></td>
+    <td><a href="#" class="fa fa-edit fa-lg text-info edit" data-id="${item.id}"></a></td>
     </tr>`;
     },
 
     displayItems: function (items) {
         console.log('Displaying items.');
         var rows = '';
+
 
         let content = items.content;
 
@@ -58,25 +62,42 @@ window.PhoneBook = {
         }).done(function (response) {
             console.log(response);
             // reload items table
-
             PhoneBook.displayItems(response)
         });
     },
 
+    deleteItem: function (id) {
+        $.ajax({
+            url: PhoneBook.apiUrl + '?id=' + id,
+            method: "DELETE"
+        }).done(function (response) {
+            console.log(response);
+            // reload items table
+            PhoneBook.getItems();
+        });
+    },
 
-    // add item
     bindEvents: function () {
-
         $("#create-item-form").submit(function (event) {
-            console.log("Submitting form 1");
             event.preventDefault();
-            console.log("Submitting form");
+
+            console.log('Submitting form');
 
             PhoneBook.addItem();
+
             return false;
+        });
+
+        $('#phone-address tbody').delegate('.delete', 'click', function () {
+            var id = $(this).data('id');
+            PhoneBook.deleteItem(id);
         });
     }
 };
-
 PhoneBook.getItems();
 PhoneBook.bindEvents();
+
+
+
+// lebenite
+// fbi
